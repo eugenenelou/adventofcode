20f37b0e93ce1dd64efb15471f064ce42f977b84
Eugène Nélou <eugene.nelou@gitguardian.com>
2023-12-13 10:56:06 +0100

    2023 day 12 - concatenate all lines together, it runs in ~2s

--- adventofcode/2023 - python/day12/run.py
+++ adventofcode/2023 - python/day12/run.py
@@ -21,8 +21,10 @@ def split_row_in_masks(row: str) -> tuple:
 
 
 @cache
-def count_row(masks: tuple, group_sizes: tuple[int]) -> int:
-    if sum(len(mask) for mask in masks) < sum(group_sizes):
+def count_row(
+    masks: tuple, group_sizes: tuple[int], mask_total: int, group_total: int
+) -> int:
+    if mask_total < group_total:
         return 0
     if len(group_sizes) == 0:
         # print("a")
@@ -32,19 +34,21 @@ def count_row(masks: tuple, group_sizes: tuple[int]) -> int:
         return 0
     mask = masks[0]
     if not mask:
-        return count_row(masks[1:], group_sizes)
+        return count_row(masks[1:], group_sizes, mask_total - len(mask), group_total)
     group_size = group_sizes[0]
 
     if len(mask) < group_size:
         return (
-            count_row(masks[1:], group_sizes)
+            count_row(masks[1:], group_sizes, mask_total - len(mask), group_total)
             if all(char == "?" for char in mask)
             else 0
         )
     if len(mask) == group_size:
         # print("b", mask)
-        return count_row(masks[1:], group_sizes[1:]) + (
-            count_row(masks[1:], group_sizes)
+        return count_row(
+            masks[1:], group_sizes[1:], mask_total - len(mask), group_total - group_size
+        ) + (
+            count_row(masks[1:], group_sizes, mask_total - len(mask), group_total)
             if all(char == "?" for char in mask)
             else 0
         )
@@ -53,54 +57,85 @@ def count_row(masks: tuple, group_sizes: tuple[int]) -> int:
         return 0
     if mask[group_size] == "?":
         # print("F")
-        return (count_row((mask[group_size + 1 :], *masks[1:]), group_sizes[1:])) + (
-            count_row((mask[1:], *masks[1:]), group_sizes) if mask[0] == "?" else 0
+        return (
+            count_row(
+                (mask[group_size + 1 :], *masks[1:]),
+                group_sizes[1:],
+                mask_total - len(mask[: group_size + 1]),
+                group_total - group_size,
+            )
+        ) + (
+            count_row(
+                (mask[1:], *masks[1:]),
+                group_sizes,
+                mask_total - len(mask[:1]),
+                group_total,
+            )
+            if mask[0] == "?"
+            else 0
         )
     else:
         # print("G")
-        return count_row((mask[1:], *masks[1:]), group_sizes) if mask[0] == "?" else 0
+        return (
+            count_row(
+                (mask[1:], *masks[1:]),
+                group_sizes,
+                mask_total - len(mask[:1]),
+                group_total,
+            )
+            if mask[0] == "?"
+            else 0
+        )
 
 
 def main1(input_):
     res = 0
     for i, (row, group_sizes) in enumerate(input_):
-        print(i)
-        count = count_row(split_row_in_masks(row), group_sizes)
+        # print(i)
+        masks = split_row_in_masks(row)
+        count = count_row(
+            masks,
+            group_sizes,
+            mask_total=sum(len(mask) for mask in masks),
+            group_total=sum(group_sizes),
+        )
         res += count
     return res
 
 
 def main2(input_):
     input2 = [
-        ("?".join(row for _ in range(5)), group_sizes * 5)
+        (
+            "?".join(row for _ in range(5)),
+            group_sizes * 5,
+        )
         for row, group_sizes in input_
     ]
+    # print("input2", input2)
     return main1(input2)
+
+
+def main3(input_):
+    """For the fun of it, concatenate every line"""
+    sys.setrecursionlimit(100_000)
+    input3 = [
+        (
+            "?".join(row for row, _ in input_),
+            tuple(size for _, group_sizes in input_ for size in group_sizes),
+        )
+    ]
+    # print("input3", input3)
+    return main1(input3)
 
 
 if __name__ == "__main__":
     input_filepath = sys.argv[1]
     second_part = len(sys.argv) > 2 and sys.argv[2] == "--two"
+    third_part = len(sys.argv) > 2 and sys.argv[2] == "--three"
     input_ = parse_input(input_filepath)
-    if second_part:
+    if third_part:
+        print(f"result: {main3(input_)}", file=sys.stdout)
+    elif second_part:
         print(f"result: {main2(input_)}", file=sys.stdout)
     else:
         print(f"result: {main1(input_)}", file=sys.stdout)
-
-
-"""
-???#? (1,2)
-#.##.
-#..##
-.#.##
-
-
-???#? (1,2)
-F -> ?#? (2,)
-
-  -> ??#? (1,2)
-    F -> #? (2)
-        B -> 1
-      -> ?#? (1,2)
-        _ -> 0
-"""
